1bfc152f49cf4b88de41dd2ee54b21f387aad547
clong desktop <chip_long@yahoo.com>
2024-06-24 00:28:54 -0600

    moved database

---
+++ gql_server/src/typeDefs/index.ts
@@ -0,0 +1,36 @@
+import { gql } from "graphql-tag";
+
+export const typeDefs = gql/* GraphQL */ `
+  type Power {
+    id: ID!
+    power: String!
+    alternateName: String
+    relatedPower: [ID]
+    rank: String
+    powerPoints: Int
+    range: String
+    duration: String
+    durationMultiplier: Int
+    summary: String
+    trappings: String
+    fullDescription: String
+    book: String
+    pageNumber: Int
+    isSpecial: Boolean
+    powerModifiers: [PowerModifier]
+  }
+
+  type PowerModifier {
+    id: ID!
+    modifierName: String
+    description: String
+    shortModifierName: String
+    cost: Int
+    takeMultiple: Boolean
+    isRequired: Boolean
+  }
+
+  type Query {
+    getPowerList: [Power]!
+  }
+`;
